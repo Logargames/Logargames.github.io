187e13f9949392dd1bc498b6d488d589c29b4353
Logargames <frayt025@gmail.com>
2026-02-17 19:00:23 +0100

    test v1

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,13 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getStorage } from 
-"https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
-export const storage = getStorage(app);
+import { getFirestore } from 
+"https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+import { getAuth } from 
+"https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+/* ========= CONFIG ========= */
 
 const firebaseConfig = {
   apiKey: "AIzaSyDh4XkMr6kNXq6rPMlpHAZhenUQgBsl0uo",
@@ -12,10 +15,15 @@ const firebaseConfig = {
   projectId: "logar-games",
   storageBucket: "logar-games.firebasestorage.app",
   messagingSenderId: "943691798770",
-  appId: "1:943691798770:web:15dca92cb1aa69d411d86b"
+  appId: "1:943691798770:web:15dca92cb1aa69d411d86b",
+  measurementId: "G-N46PWQFJZF"
 };
 
+/* ========= INITIALISATION ========= */
+
 const app = initializeApp(firebaseConfig);
+
+/* ========= EXPORTS ========= */
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
